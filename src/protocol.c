0456bf2538d27842386dd4bc30951fcc792a6aca
Cesar Sepulveda <cesar.sepulveda.b@gmail.com>
2025-06-13 23:01:24 -0400

    coderabbit remediations

--- src/protocol.c
+++ src/protocol.c
@@ -30,8 +30,7 @@ command_t parse_command(const char *message) {
     };
 
     const size_t num_commands = sizeof(commands) / sizeof(commands[0]);
-    size_t msg_len = message ? strlen(message) : 0;
-    
+    size_t msg_len = strlen(message); //NOSONAR
     for (size_t i = 0; i < num_commands; i++) {
         const command_def_t *c = &commands[i];
         if (msg_len < c->len) continue;             /* avoid OOB */
